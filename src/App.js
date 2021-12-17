799b4bace6100accd7ba5b5fa844b60033f0473e
Edgar Alfonso Lopez De Los Santos <edgaralfonsolopezdelossantos@Edgars-MacBook-Pro.local>
2021-12-17 10:23:53 -0800

    created company useStates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [invoice, setInvoice] = useState(true)
+  const [companyName, setCompanyName] = useState('Company name here')
+  const [companyAddress, setCompanyAddress] = useState('Company Address')
+  const [companyEmail, setCompanyEmail] = useState('Company Email')
+  const [companyPhone,setCompanyPhone] =useState('Company Phone #')
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <p>{companyName}</p> 
+        <p>{companyAddress}</p>
+     
     </div>
   );
 }
