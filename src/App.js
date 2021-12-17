c82aa971ae42d742d893e3a7cf2994aeae061a65
Edgar Alfonso Lopez De Los Santos <edgaralfonsolopezdelossantos@Edgars-MacBook-Pro.local>
2021-12-17 14:59:45 -0800

    Changed useStates back to individual states also added fields to change company name/address

--- src/App.js
+++ src/App.js
@@ -4,39 +4,39 @@ import { useState } from 'react';
 function App() {
 
   const [invoice, setInvoice] = useState(true)
-  const [companyInfo, setCompanyInfo] = useState({
-    companyName: 'CompanyName', 
-    companyAddress: 'CompanyAddress', 
-    companyEmail: 'CompanyEmail', 
-    companyPhone: 'CompanyPhone'})
-  // const [companyName, setCompanyName] = useState('Company name here')
-  // const [companyAddress, setCompanyAddress] = useState('Company Address')
-  // const [companyEmail, setCompanyEmail] = useState('Company Email')
-  // const [companyPhone,setCompanyPhone] =useState('Company Phone #')
-  const [clientInfo, setClientInfo] = useState({
-    clientName: 'ClientName',
-    clientAddress:  'ClientAddress'
-  })
-  // const [clientName, setClientName] = useState('Client Name')
-  // const [clientAddress, setClientAddress] = useState('Client Address')
-  const [invoiceInfo, setInvoiceInfo] = useState({
-    invoiceNumber: '0',
-    invoiceDate: '12/17/2021',
-    sentDate: '12/17/2021'
-  })
-  // const [invoiceNumber, setInvoiceNumber] = useState('0')
-  // const [invoiceDate, setInvoiceDate] = useState('12/17/2021')
-  // const [sentDate, setSentDate] = useState('12/17/2021')
-  const [work, setWork] = useState({
-    description: '',
-    price: '',
-    amount: '',
-    total: 0
-  })
-  // const [description, setDescription] = useState('')
-  // const [price, setPrice] = useState('')
-  // const [amount, setAmount] = useState('')
-  // const [total, setTotal] = useState(0)
+  // const [companyInfo, setCompanyInfo] = useState({
+  //   companyName: 'CompanyName', 
+  //   companyAddress: 'CompanyAddress', 
+  //   companyEmail: 'CompanyEmail', 
+  //   companyPhone: 'CompanyPhone'})
+  const [companyName, setCompanyName] = useState('Company name here')
+  const [companyAddress, setCompanyAddress] = useState('Company Address')
+  const [companyEmail, setCompanyEmail] = useState('Company Email')
+  const [companyPhone,setCompanyPhone] =useState('Company Phone #')
+  // const [clientInfo, setClientInfo] = useState({
+  //   clientName: 'ClientName',
+  //   clientAddress:  'ClientAddress'
+  // })
+  const [clientName, setClientName] = useState('Client Name')
+  const [clientAddress, setClientAddress] = useState('Client Address')
+  // const [invoiceInfo, setInvoiceInfo] = useState({
+  //   invoiceNumber: '0',
+  //   invoiceDate: '12/17/2021',
+  //   sentDate: '12/17/2021'
+  // })
+  const [invoiceNumber, setInvoiceNumber] = useState('0')
+  const [invoiceDate, setInvoiceDate] = useState('12/17/2021')
+  const [sentDate, setSentDate] = useState('12/17/2021')
+  // const [work, setWork] = useState({
+  //   description: '',
+  //   price: '',
+  //   amount: '',
+  //   total: 0
+  // })
+  const [description, setDescription] = useState('')
+  const [price, setPrice] = useState('')
+  const [amount, setAmount] = useState('')
+  const [total, setTotal] = useState(0)
   const [list, setList] = useState([])
 
   return (
@@ -44,24 +44,24 @@ function App() {
     <div>
     {invoice ? (
         <div className="App">
-        <p>{companyInfo.companyName}</p> 
-        <p>{companyInfo.companyAddress}</p>
-        <p>{companyInfo.companyEmail}</p>
-        <p>{companyInfo.companyPhone}</p>
+        <p>{companyName}</p> 
+        <p>{companyAddress}</p>
+        <p>{companyEmail}</p>
+        <p>{companyPhone}</p>
 
-        <p>{clientInfo.clientName}</p> 
-        <p>{clientInfo.clientAddress}</p>
-
-
-        <p>{invoiceInfo.invoiceNumber}</p>
-        <p>{invoiceInfo.invoiceDate}</p>
-        <p>{invoiceInfo.sentDate}</p>
+        <p>{clientName}</p> 
+        <p>{clientAddress}</p>
 
 
-        <p>{work.description}</p>
-        <p>{work.price}</p>
-        <p>{work.amount}</p>
-        <p>{work.total}</p>
+        <p>{invoiceDate}</p>
+        <p>{invoiceNumber}</p>
+        <p>{sentDate}</p>
+
+
+        <p>{description}</p>
+        <p>{price}</p>
+        <p>{amount}</p>
+        <p>{total}</p>
         <p>{list}</p>
 
         <button 
@@ -71,10 +71,33 @@ function App() {
         </div>
       ):(
         <div>
+          <article>
+            <div>
+              <label>CompanyName</label>
+              <input
+                type="text"
+                name="text"
+                placeholder="CompanyName"
+                value={companyName}
+                onChange={(e) => setCompanyName(e.target.value)}
+              />
+            </div>
+
+            <div>
+              <label>CompanyAddress</label>
+              <input
+                type="text"
+                name="text"
+                placeholder="CompanyAddress"
+                value={companyAddress}
+                onChange={(e) => setCompanyAddress(e.target.value)}
+              />
+            </div>
+          </article>
           <h1>Edit section</h1>
           <button 
         onClick={()=> setInvoice(true)}>
-          Edit Info
+          Preview Invoice
         </button>
         </div>
       )}
