68f92dca3c98a50809fefe718f503e79e62ff86e
Edgar Alfonso Lopez De Los Santos <edgaralfonsolopezdelossantos@Edgars-MacBook-Pro.local>
2021-12-20 11:25:27 -0800

    Reat-Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import './App.css';
 import { useState } from 'react';
+import { Card, Container } from 'react-bootstrap';
 
 function App() {
 
@@ -39,17 +40,24 @@ function App() {
   const [total, setTotal] = useState(0)
   const [list, setList] = useState([])
 
+  const handlePrint = () => {
+    window.print()
+  }  
   return (
     <> 
-    <div>
+    {/* <div className="m-5 p-5 md:max-w-xl md:mx-auto lg:max-w-2xl xl:max-w-4xl bg-white rounded shadow">
     {invoice ? (
-        <div className="App">
-        <p>{companyName}</p> 
+
+        <div >
+          <section>
+        <h2>{companyName}</h2> 
         <p>{companyAddress}</p>
         <p>{companyEmail}</p>
         <p>{companyPhone}</p>
 
-        <p>{clientName}</p> 
+          </section>
+
+        <p >{clientName}</p> 
         <p>{clientAddress}</p>
 
 
@@ -67,6 +75,11 @@ function App() {
         <button 
         onClick={()=> setInvoice(false)}>
           Edit Info
+        </button>
+
+        <button 
+        onClick={handlePrint}>
+          Print
         </button>
         </div>
       ):(
@@ -185,9 +198,162 @@ function App() {
         </button>
         </div>
       )}
-      </div>
+      </div> */}
+       <Container>
+        <Card>
+          <Card.Body>
+            <div id="invoice">
+              <div className="toolbar hidden-print">
+                <div className="text-end">
+                  <button type="button" className="dark">
+                    <i className="fa fa-print"></i> Print
+                  </button>
+                  <button type="button" className="danger">
+                    <i className="fa fa-file-pdf-o"></i> Export as PDF
+                  </button>
+                </div>
+                <hr />
+              </div>
+              <div className="invoice overflow-auto">
+                <div style={{minWidth: 600}}>
+                  <header>
+                    <div className="row">
+                      <div className="col">
+                        <a href="javascript;">
+                          <img
+                            src="assets/images/logo-icon.png"
+                            width="80"
+                            alt=""
+                          />
+                        </a>
+                      </div>
+                      <div className="col company-details">
+                        <h2 className="name">
+                          <a target="_blank" href="javascript;">
+                            {companyName}
+                          </a>
+                        </h2>
+                        <div>{companyAddress}</div>
+                        <div>{companyPhone}</div>
+                        <div>{companyEmail}</div>
+                      </div>
+                    </div>
+                  </header>
+                  <main>
+                    <div className="row contacts">
+                      <div className="col invoice-to">
+                        <div className="text-gray-light">INVOICE TO:</div>
+                        <h2 className="to">{clientName}</h2>
+                        <div className="address">
+                          {clientAddress}
+                        </div>
+                      </div>
+                      <div className="col invoice-details">
+                        <h1 className="invoice-id">{invoiceNumber}</h1>
+                        <div className="date">{invoiceDate}</div>
+                        <div className="date">{sentDate}</div>
+                      </div>
+                    </div>
+                    <table>
+                      <thead>
+                        <tr>
+                          <th>#</th>
+                          <th className="text-left">DESCRIPTION</th>
+                          <th className="text-right">HOUR PRICE</th>
+                          <th className="text-right">HOURS</th>
+                          <th className="text-right">TOTAL</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td className="no">04</td>
+                          <td className="text-left">
+                            <h3>
+                              <a target="_blank" href="javascript;">
+                                Youtube channel
+                              </a>
+                            </h3>
+                            <a target="_blank" href="javascript;">
+                              Useful videos
+                            </a>{' '}
+                          </td>
+                          <td className="unit">$0.00</td>
+                          <td className="qty">100</td>
+                          <td className="total">$0.00</td>
+                        </tr>
+                        <tr>
+                          <td className="no">01</td>
+                          <td className="text-left">
+                            <h3>Website Design</h3>Creating a recognizable
+                            design solution based on the company's existing
+                            visual identity
+                          </td>
+                          <td className="unit">$40.00</td>
+                          <td className="qty">30</td>
+                          <td className="total">$1,200.00</td>
+                        </tr>
+                        <tr>
+                          <td className="no">02</td>
+                          <td className="text-left">
+                            <h3>Website Development</h3>Developing a Content
+                            Management System-based Website
+                          </td>
+                          <td className="unit">$40.00</td>
+                          <td className="qty">80</td>
+                          <td className="total">$3,200.00</td>
+                        </tr>
+                        <tr>
+                          <td className="no">03</td>
+                          <td className="text-left">
+                            <h3>Search Engines Optimization</h3>Optimize the
+                            site for search engines (SEO)
+                          </td>
+                          <td className="unit">$40.00</td>
+                          <td className="qty">20</td>
+                          <td className="total">$800.00</td>
+                        </tr>
+                      </tbody>
+                      <tfoot>
+                        <tr>
+                          <td colspan="2"></td>
+                          <td colspan="2">SUBTOTAL</td>
+                          <td>$5,200.00</td>
+                        </tr>
+                        <tr>
+                          <td colspan="2"></td>
+                          <td colspan="2">TAX 25%</td>
+                          <td>$1,300.00</td>
+                        </tr>
+                        <tr>
+                          <td colspan="2"></td>
+                          <td colspan="2">GRAND TOTAL</td>
+                          <td>$6,500.00</td>
+                        </tr>
+                      </tfoot>
+                    </table>
+                    <div className="thanks">Thank you!</div>
+                    <div className="notices">
+                      <div>NOTICE:</div>
+                      <div className="notice">
+                        A finance charge of 1.5% will be made on unpaid balances
+                        after 30 days.
+                      </div>
+                    </div>
+                  </main>
+                  <footer>
+                    Invoice was created on a computer and is valid without the
+                    signature and seal.
+                  </footer>
+                </div>
+                <div></div>
+              </div>
+            </div>
+          </Card.Body>
+        </Card>
+      </Container>
     </> 
   );
 }
 
 export default App;
+
